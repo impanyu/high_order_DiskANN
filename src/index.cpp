5c1ebff9edbcbd9a52d9f1c06db35c3db44d354d
impanyu <impanyu@gmail.com>
2024-02-02 20:58:16 -0600

    test

--- src/index.cpp
+++ src/index.cpp
@@ -1209,7 +1209,7 @@ void Index<T, TagT, LabelT>::occlude_list(const uint32_t location, std::vector<N
         }
            std::cout<<alphas.size()<<"here";
            std::cout<<layer[min_id]<<"here";
-           std::cout alphas[layer[min_id]];
+           std::cout<<alphas[layer[min_id]];
            std::cout << std::endl;
         //modify C and E based on current layer of min_id
         for (auto iter = pool.begin();  iter != pool.end(); ++iter){
