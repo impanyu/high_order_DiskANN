49262ef17ccf19eef330b70a701cb81727b060a5
impanyu <impanyu@gmail.com>
2024-02-23 11:44:56 -0600

    test

--- src/index.cpp
+++ src/index.cpp
@@ -1335,7 +1335,7 @@ void Index<T, TagT, LabelT>::occlude_list(const uint32_t location, std::vector<N
                 neighbour_with_indices[j][1] /= (pool.size() - result.size()-1);
             }
 
-            
+            /*
             if (result.size() ==1){
                 neighbour_with_indices[j][2] = d_j/(d_i_j+1e-6);
             }
@@ -1343,7 +1343,7 @@ void Index<T, TagT, LabelT>::occlude_list(const uint32_t location, std::vector<N
                 neighbour_with_indices[j][2] *= result.size()-1;
                 neighbour_with_indices[j][2] += d_j/(d_i_j+1e-6);
                 neighbour_with_indices[j][2] /= result.size();
-            }
+            }*/
             
         }
     }
