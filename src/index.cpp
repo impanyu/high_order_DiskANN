81e4a28c9436a0685f8ef2dd6a091e0281d5b8aa
impanyu <impanyu@gmail.com>
2024-03-02 18:19:54 -0600

    test

--- src/index.cpp
+++ src/index.cpp
@@ -1188,7 +1188,7 @@ void Index<T, TagT, LabelT>::occlude_list(const uint32_t location, std::vector<N
               cur_alpha2 = _indexingAlphas[1];
         else
               cur_alpha2 = 1.2;*/
-        for(; cur_alpha2 >=.9; cur_alpha2 = cur_alpha2 -0.1){
+        for(; cur_alpha >=1; cur_alpha = cur_alpha -0.02){
           
         
 
